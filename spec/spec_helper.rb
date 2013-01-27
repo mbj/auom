4aec214263a117feb3369193788ccdfe1eff74b0
Markus Schirp <mbj@seonic.net>
2013-01-27 17:33:17 +0100

    Fix rcov under 1.8

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,4 +5,8 @@ $LOAD_PATH << File.expand_path('../lib', __FILE__)
 Dir.glob('spec/examples/**/*.rb').each { |file| require File.expand_path(file) }
 Dir[File.expand_path('../{support,shared}/**/*.rb', __FILE__)].each { |f| require f }
 
+if RUBY_VERSION < '1.9'
+  require 'rspec/autorun'
+end
+
 require 'auom'
